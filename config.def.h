3ec38623b264828623f2800768d371a37a727edc
Mr.X <abas93931@gmail.com>
2023-08-17 06:42:48 +0300

    Made top bare more transparent

--- config.def.h
+++ config.def.h
@@ -17,7 +17,7 @@ static       Bool bUseOpacity       = True;     /* Starts with opacity on any un
 
 static const char *fonts[]          = { "FiraCode Nerd Font:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
